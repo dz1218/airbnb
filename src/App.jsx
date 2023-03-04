27a683dcd7b2ee857074f40bb6a187db96fc1777
dz1218 <690854045@qq.com>
2023-03-05 00:58:23 +0800

    feat: win使用双引号

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@
 // });
 
 // export default App;
-import React, { memo } from 'react'
-import { useRoutes } from 'react-router-dom'
-import routes from './router'
+import React, { memo } from "react"
+import { useRoutes } from "react-router-dom"
+import routes from "./router"
 
 const App = memo(() => {
   return (
