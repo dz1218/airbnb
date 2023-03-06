cbf368c7ebdee924b63ac7c19c585bc9b0d6b548
dz1218 <690854045@qq.com>
2023-03-06 22:49:35 +0800

    feat: 引入图片资源

---
+++ src/views/home/homeCpns/homeBanner/style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+import homeBannerCoverImg from "@/assets/img/cover_01.jpeg"
+
+export const HomeBannerWrapper = styled.div`
+  height: 529px;
+  background: url(${homeBannerCoverImg}) center/cover;
+`
