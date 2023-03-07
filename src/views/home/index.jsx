7d1c9b28ed4867370e1e610ede19e608782a04d9
dz1218 <690854045@qq.com>
2023-03-07 21:40:45 +0800

    feat: mac更新

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,26 +1,13 @@
-import hyRequest from "@/service"
 import { memo, useEffect, useState } from "react"
+import HomeBanner from "./homeCpns/homeBanner"
+import { HomeWrapper } from "./style"
 
 const Home = memo(() => {
-  const [highScore, sethighScore] = useState([])
-
-  useEffect(() => {
-    hyRequest
-      .get({
-        url: "/home/highScore"
-      })
-      .then((res) => {
-        sethighScore(res.list)
-        // console.log(highScore)
-      })
-  })
-
   return (
-    <div>
-      {highScore.map((item) => (
-        <div>{item.name}</div>
-      ))}
-    </div>
+    <HomeWrapper>
+      <HomeBanner />
+      <div className="home-content">11</div>
+    </HomeWrapper>
   )
 })
 
