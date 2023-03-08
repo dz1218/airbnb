74247344406c7676afc583bb629f69742102296d
dz1218 <690854045@qq.com>
2023-03-08 22:26:25 +0800

    feat: win提交3/8

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,14 +1,22 @@
-import { memo, useEffect, useState } from "react"
-import HomeBanner from "./homeCpns/homeBanner"
-import { HomeWrapper } from "./style"
+import { fetchHomeDataAction } from "@/store/modules/home";
+import { memo, useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import HomeBanner from "./homeCpns/homeBanner";
+import { HomeWrapper } from "./style";
 
 const Home = memo(() => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHomeDataAction());
+  }, [dispatch]);
+
   return (
     <HomeWrapper>
       <HomeBanner />
-      <div className="home-content">11</div>
+      <div className="home-content"></div>
     </HomeWrapper>
-  )
-})
+  );
+});
 
-export default Home
+export default Home;
