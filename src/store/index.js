cb8984109f2451d8036a8f018694553c4ccc2903
dz1218 <690854045@qq.com>
2023-03-08 22:26:25 +0800

    feat: win提交3/8

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import homeReducer from "./modules/home";
-import entireReducer from "./modules/entire";
 
 const store = configureStore({
   reducer: {
-    home: homeReducer,
-    entire: entireReducer,
-  },
+    home: homeReducer
+  }
 });
 
 export default store;
